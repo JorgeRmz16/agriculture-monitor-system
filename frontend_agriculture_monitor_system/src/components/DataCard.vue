4423b8d04422deaf818834447a7e493af9248516
Jorge Ramirez <jorgermzc16@gmail.com>
2025-08-01 14:27:35 -0600

    Frontend basse structure

---
+++ frontend_agriculture_monitor_system/src/components/DataCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <div class="icon-wrapper">
+      <i :class="icon"></i>
+    </div>
+    <div class="data-wrapper">
+      <h2>{{ title }}</h2>
+      <p class="value">
+        {{ value }} <span class="unit">{{ unit }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  icon: { type: String, required: true },
+  title: { type: String, required: true },
+  value: { type: [String, Number], required: true },
+  unit: { type: String, required: true }
+});
+</script>
